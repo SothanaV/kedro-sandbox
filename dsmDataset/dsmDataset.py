d89b3f38b21fe5adf8655d24df804e5d4d2706a7
SothanaV <nai6320@gmail.com>
2024-05-16 17:01:39 +0700

    [UPDATE] load & save dataset

--- dsmDataset/dsmDataset.py
+++ dsmDataset/dsmDataset.py
@@ -1,46 +1,58 @@
 from typing import Any, Dict, Tuple, List
 from kedro.io import AbstractDataset
 
+import json
+import datetime
+
 import dask.dataframe as dd
 from dask.diagnostics import ProgressBar
 
-from . import utils
-
-import json
+from . import utils, customS3filesystem
 
 class DaskDataset(AbstractDataset[dd.DataFrame, None]):
-    def __init__(self, credentials: Dict[str, Any], name: str, directory: str, save_args: dict = {}, read_args:dict = {}):
+    def __init__(self, credentials: Dict[str, Any], name: str, directory: str, mode:str="overwrite",save_args: dict = {}, read_args:dict = {}):
         utils.creadentials.check_creadentials(creadentials=credentials)
-        self._storage_options = {
-            'key': credentials.get('access_key'),
-            'secret': credentials.get('secret_key'),
-            'client_kwargs':{
-                'endpoint_url': credentials.get('uri')
-            },
-            'use_listings_cache': False,
-            'default_fill_cache': False,
-        }
         self.name = name
-        self.path = f"s3://{credentials.get('bucket_name')}/{credentials.get('project')}/{directory}/{name}"
+        self.path = f"{credentials.get('bucket_name')}/{credentials.get('project')}@project/{directory}/{name}@parquet"
         self._save_args = save_args
         self._read_args = read_args
+        self._mode = mode
+
+        self._bucket = customS3filesystem.CustomS3filesystem(
+            endpoint_url=credentials.get('uri'), 
+            key=credentials.get('access_key'), 
+            secret=credentials.get('secret_key'),
+            use_listings_cache=False,
+            default_fill_cache=False,
+        )
+        self._save_args.update({
+            'name_function': lambda x: f"part-{datetime.datetime.now().isoformat()}.parquet",
+            'ignore_divisions': True,
+            'overwrite': True
+        })
 
     def _describe(self) -> Dict[str, Any]:
         pass
 
     def _load(self) -> Dict[dd.DataFrame, Any]:
-        return dd.read_parquet(path=self.path, storage_options=self._storage_options, **self._read_args)
+        return dd.read_parquet(path=self.path, filesystem=self._bucket, **self._read_args)
 
     def _save(self, data: Tuple[dd.DataFrame, List[int]]) -> None:
         ddf, meta_list = data
-        meta = {
-            'datadict': json.dumps({str(k):str(v) for k,v in dict(ddf.dtypes).items()})
-        }
+        if self._mode == 'append' and self._bucket.exists(self.path):
+            self._save_args.update({'append': True})
+            if 'overwrite' in self._save_args:
+                self._save_args.pop('overwrite')
         with ProgressBar():
             ddf.to_parquet(
-                path=self.path, 
-                storage_options=self._storage_options, 
+                path=self.path,
+                filesystem=self._bucket, 
                 engine="pyarrow",
-                custom_metadata=meta,
                 **self._save_args,
             )
+        meta = {
+            'datadict': utils.handler.handle_timeout(function=utils.dataframe.get_datadict, args={'ddf': ddf}, default_value=[]),
+            'example': utils.handler.handle_timeout(function=utils.dataframe.get_data_example, args={'ddf': ddf}, default_value=[])
+        }
+        with self._bucket.open(f"{self.path}.metadata.json", 'w') as f:
+            json.dump(obj=meta, fp=f)
